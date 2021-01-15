8ef367316ae147d52da2b0f46fd806891857ba53
geoyk <geoyking@gmail.com>
2021-01-16 00:48:30 +0200

    Two cats (Lol)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 mod head;
+mod body;
 
 fn main() {
     // Kitty Head ( LOL )
     head::ears();
     head::main_head();
+
+    // Body ( LOL )
+    body::main_body();
+
+    println!("KAT N.2");
+    body::full_cat2_body();
 }
