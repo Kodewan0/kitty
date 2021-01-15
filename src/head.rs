ec36adf422203f2c8247ef232a8f22f148a2a4ed
geoyk <geoyking@gmail.com>
2021-01-15 20:04:57 +0200

    head commit!

---
+++ src/head.rs
@@ -0,0 +1,12 @@
+pub fn ears() {
+    println!("　▕╲　　　╱▏");
+    println!("　▕　╲▁▁╱　▏");
+}
+
+pub fn main_head() {
+    println!("　▕┏┳╮ ╭┳┓▏");
+    println!("　▕╰┻┫ ┣┻╯▏");
+    println!("　▕╱╱◥◤╲╲");
+    println!("　　╲　━━　╱╭━━╮");
+    println!("　　┃╲▁▁╱┃┃╭━╯");
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod head;
+
+fn main() {
+    head::ears();
+    head::main_head();
+}
